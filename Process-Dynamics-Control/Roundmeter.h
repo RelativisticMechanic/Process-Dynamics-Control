67323cd65b4ad89f4a001b5aae495373e7af5ff8
RelativisticMechanic <cyclicintegral@gmail.com>
2023-01-23 22:05:42 +0530

    Clean repo

---
+++ Process-Dynamics-Control/Roundmeter.h
@@ -0,0 +1,22 @@
+#ifndef ROUNDMETER_H
+#define ROUNDMETER_H
+
+#include "Graph2D.h"
+
+class RoundMeter
+{
+public:
+	olc::PixelGameEngine* app;
+	double x;
+	double xmin, xmax;
+
+	vec2 position;
+	vec2 dimensions;
+
+	RoundMeter() { };
+	RoundMeter(olc::PixelGameEngine* app, vec2 position, vec2 dimensions, double x, double xmin, double xmax);
+	void Update(double x);
+	void Draw();
+};
+
+#endif
